57680e2c2f814e545af80da9e381baaff531cf33
avantwrks <david.avilarodriguez@hotmail.com>
2025-05-07 23:04:11 -0500

    Entrega de la tarea

--- main.cpp
+++ main.cpp
@@ -1,127 +1,62 @@
 #include <iostream>
-#include <string>
 #include <vector>
 #include <sstream>
-using namespace std;
-
-// Clase base
-class MagicalCreature {
-protected:
-    string name;
-    int health;
-    int attackPower;
-    int defensePower;
-
-public:
-    MagicalCreature(string n, int h, int atk, int def)
-        : name(n), health(h), attackPower(atk), defensePower(def) {}
-
-    virtual void showInfo() const {
-        cout << "Nombre: " << name << ", Vida: " << health
-             << ", Ataque: " << attackPower
-             << ", Defensa: " << defensePower << endl;
-    }
-
-    virtual void attack(MagicalCreature* target, int power) {
-        int damage = power - target->defensePower;
-        if (damage < 0) damage = 0;
-        target->health -= damage;
-        if (target->health < 0) target->health = 0;
-        cout << name << " ataca a " << target->name << " causando " << damage << " de dano." << endl;
-    }
-
-    string getName() const {
-        return name;
-    }
-
-    virtual ~MagicalCreature() {}
-};
-
-// Subclases
-class Dragon : public MagicalCreature {
-public:
-    Dragon(string n, int h, int atk, int def)
-        : MagicalCreature(n, h, atk, def) {}
-
-    void showInfo() const override {
-        cout << "[Dragon] ";
-        MagicalCreature::showInfo();
-    }
-};
-
-class Hada : public MagicalCreature {
-public:
-    Hada(string n, int h, int atk, int def)
-        : MagicalCreature(n, h, atk, def) {}
-
-    void showInfo() const override {
-        cout << "[Hada] ";
-        MagicalCreature::showInfo();
-    }
-};
-
-// Buscar criatura por nombre
-MagicalCreature* findCreature(const string& name, const vector<MagicalCreature*>& creatures) {
-    for (MagicalCreature* c : creatures) {
-        if (c->getName() == name)
-            return c;
-    }
-    return nullptr;
-}
+#include "Dragon.h"
+#include "Hada.h"
 
 int main() {
-    vector<MagicalCreature*> creatures;
-    string line;
+    std::vector<MagicalCreature*> criaturas;
+    std::string linea;
 
-    while (true) {
-        getline(cin, line);
-        stringstream ss(line);
-        string command;
-        ss >> command;
+    while (std::getline(std::cin, linea)) {
+        std::stringstream ss(linea);
+        std::string comando;
+        ss >> comando;
 
-        if (command == "CREA_DRAGON") {
-            string name;
-            int h, atk, def;
-            ss >> name >> h >> atk >> def;
-            creatures.push_back(new Dragon(name, h, atk, def));
+        if (comando == "CREA_DRAGON") {
+            std::string nombre;
+            int vida, ataque, defensa;
+            ss >> nombre >> vida >> ataque >> defensa;
+            criaturas.push_back(new Dragon(nombre, vida, ataque, defensa));
         }
-        else if (command == "CREA_HADA") {
-            string name;
-            int h, atk, def;
-            ss >> name >> h >> atk >> def;
-            creatures.push_back(new Hada(name, h, atk, def));
+        else if (comando == "CREA_HADA") {
+            std::string nombre;
+            int vida, ataque, defensa;
+            ss >> nombre >> vida >> ataque >> defensa;
+            criaturas.push_back(new Hada(nombre, vida, ataque, defensa));
         }
-        else if (command == "MOSTRAR") {
-            for (MagicalCreature* c : creatures) {
-                c->showInfo();
+        else if (comando == "MOSTRAR") {
+            for (auto criatura : criaturas) {
+                criatura->showStatus();
             }
         }
-        else if (command == "ATACA") {
-            string attackerName, targetName;
-            int power;
-            ss >> attackerName >> targetName >> power;
+        else if (comando == "ATACA") {
+            std::string atacante, objetivo;
+            int poder;
+            ss >> atacante >> objetivo >> poder;
 
-            MagicalCreature* attacker = findCreature(attackerName, creatures);
-            MagicalCreature* target = findCreature(targetName, creatures);
+            MagicalCreature* atacador = nullptr;
+            MagicalCreature* victima = nullptr;
 
-            if (attacker && target && attacker != target) {
-                attacker->attack(target, power);
-            } else {
-                cout << "Atacante o objetivo no valido.\n";
+            for (auto criatura : criaturas) {
+                if (criatura->getName() == atacante) atacador = criatura;
+                if (criatura->getName() == objetivo) victima = criatura;
+            }
+
+            if (atacador && victima) {
+                atacador->attack(victima, poder);
             }
         }
-        else if (command == "SALIR") {
-            cout << "Saliendo...\n";
+        else if (comando == "SALIR") {
             break;
         }
         else {
-            cout << "Comando invalido.\n";
+            std::cout << "Opcion invalida." << std::endl;
         }
     }
 
-    // Liberar memoria
-    for (MagicalCreature* c : creatures) {
-        delete c;
+    for (auto criatura : criaturas) {
+        delete criatura;
     }
 
     return 0;
